2a824ae33e37da213e044c82a3069299bbb8f7d2
IngloriousRalle <ralf.willrich@web.de>
2025-01-03 10:20:04 +0100

    123

---
+++ src/src/src/02_variablen.js
@@ -0,0 +1,19 @@
+//  Zeilenkommentar
+
+/*
+    Blockkommentar  
+ */
+/***** Variablen 01 *******/
+
+/* Deklaration + Wertzuweisung I */
+
+// Deklaration (Definition)
+// Ausgabe
+
+// Wertzuweisung | assignment
+let firstName; // Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max"; // Wertzuweisung | assignment
+console.log(firstName); // Ausgabe
+let familiyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log("Hi, ich bin " + firstName + " " + familiyName + "!"); 
